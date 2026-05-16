cf4221eb775d7213b6ac956334dd95677c967fb8
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-16 21:07:10 +0300

    Implement TupleMeta, SuperTuple, Exercise

--- supertuple.py
+++ supertuple.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+import operator
 
 
 class TupleMeta(type):
-    pass
+    def __init__(cls, clsname, bases, clsdict):
+        fields = clsdict.get("_fields", [])
+        for n, name in enumerate(fields):
+            setattr(cls, name, property(operator.itemgetter(n)))
+        super().__init__(clsname, bases, clsdict)
 
 
-class SuperTuple(metclass=TupleMeta):
-    pass
+class SuperTuple(tuple, metaclass=TupleMeta):
+    def __new__(cls, *args):
+        if len(args) != len(cls._fields):
+            raise ValueError(f"Expected {len(cls._fields)} , got {len(args)}")
+        return super().__new__(cls, args)
+
+
+class Exercise(SuperTuple):
+    _fields = ["name", "weight", "reps"]
 
 
 if __name__ == "__main__":
-    st = SuperTuple(1, "asdf", 23.5)
-    print(st)
+    st = Exercise("squat", 87.5, 3)
+    print(st.name, st.weight, st.reps)
