add99c6c9435292313a982bfb50c35b2b595ce00
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-17 19:18:51 +0300

    Change Exercise to dataclass

--- cachedmeta.py
+++ cachedmeta.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 from collections import defaultdict
+from dataclasses import dataclass
 
 
 class CachedMeta(type):
@@ -15,12 +16,21 @@ class CachedMeta(type):
         return type(cls)._cached[cls][tup]
 
 
+# class Exercise(metaclass=CachedMeta):
+#     def __init__(self, name, weight, reps):
+#         self.name = name
+#         self.weight = weight
+#         self.reps = reps
+#         print(f"{self} initialized")
+
+
+#     def __repr__(self):
+#         return f"Exercise({self.name}, {self.weight}, {self.reps})"
+@dataclass
 class Exercise(metaclass=CachedMeta):
-    def __init__(self, name, weight, reps):
-        self.name = name
-        self.weight = weight
-        self.reps = reps
-        print(f"{self} initialized")
+    name: str
+    weight: float
+    reps: int
 
     def __repr__(self):
         return f"Exercise({self.name}, {self.weight}, {self.reps})"
@@ -30,5 +40,6 @@ if __name__ == "__main__":
     ex1 = Exercise("squat", 87.5, 3)
     ex2 = Exercise("squat", 87.5, 3)
     ex3 = Exercise("squat", 90.0, 2)
+    print(ex1, ex2, ex3)
     assert ex1 is ex2
     assert ex3 is not ex2
